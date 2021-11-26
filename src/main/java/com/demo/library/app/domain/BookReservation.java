bb827e06a52728c7b18b443ccdb5a0958a3defa9
kcri <67crystalk@naver.com>
2021-11-26 22:09:23 +0900

    refactor : 도메인 수정

--- src/main/java/com/demo/library/app/domain/BookReservation.java
+++ src/main/java/com/demo/library/app/domain/BookReservation.java
@@ -1,4 +1,16 @@
 package com.demo.library.app.domain;
 
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class BookReservation extends BaseEntity{
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name="book_id"))
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = @ForeignKey(name="reservation_id"))
+    private Reservation reservation;
 }
